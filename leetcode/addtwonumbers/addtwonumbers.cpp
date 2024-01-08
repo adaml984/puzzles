a3118587b2f7ec75b3f5b0deeeaad7ec37114088
Adam Łamarz <adam.lamarz@outlook.com>
2024-01-08 20:33:37 +0100

    addtwonumbers - leetcode - done

--- leetcode/addtwonumbers/addtwonumbers.cpp
+++ leetcode/addtwonumbers/addtwonumbers.cpp
@@ -20,7 +20,42 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode *result = new ListNode();
+        ListNode *current = result;
+        ListNode *l1next = l1, *l2next = l2;
+        bool isCarried = false;
+        int val1 = 0, val2 = 0;
+        do{
+            if(l1next != nullptr){
+                val1 = l1next->val;
+                l1next = l1next->next;
+            }else{
+                val1 = 0;
+                l1next = nullptr;
+            }
+            if(l2next != nullptr){
+                val2 = l2next->val;
+                l2next = l2next->next;
+            }else{
+                val2 = 0;
+                l2next = nullptr;
+            }
+            int val = val1 + val2;
+            if(isCarried){
+                val += 1;
+                isCarried = false;
+            }
+            current->val = val % 10;
+            if(val > 9){
+                isCarried = true;
+            }
+            
+            if(l1next == nullptr && l2next == nullptr && !isCarried){
+                break;
+            }
 
+            current->next = new ListNode();
+            current = current->next;
+        }while(true);
 
         return result;
     }
